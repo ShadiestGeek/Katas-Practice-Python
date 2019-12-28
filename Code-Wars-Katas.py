a9fbe68f2d0ddd5d171775c7c678ae4c6111a33f
Chase Tisdale <ShadiestGeek@gmail.com>
2019-12-28 12:15:03 -0500

    Added Lvl 7 Kata and Solution

--- Code-Wars-Katas.py
+++ Code-Wars-Katas.py
@@ -1,8 +1,15 @@
-Test
+#Solutions to level 8 and 7 Katas
+#NOTE: Increasing levels move in Decending Order
 
-#To Convert a Number to a String
+#To Convert a Number to a String (8)
     def number_to_string(num):
     x = str(num)
     return x
     
- 
+ #Find and remove lowest veriable within a string/list, do not mutate original list (7)
+    def remove_smallest(numbers):
+        x = list(numbers)
+        if not numbers:
+            return []
+        x.remove(min(x))
+        return x
